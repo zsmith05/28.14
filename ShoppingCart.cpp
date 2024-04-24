5880080efc8e8bd1526aa6fd0988cb416f8de1c3
carriscabaker <167334430+carriscabaker@users.noreply.github.com>
2024-04-24 13:19:30 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,1 +1,21 @@
 /* Type your code here */
+
+#include "ShoppingCart.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+ShoppingCart::ShoppingCart()
+{
+  customerName = "";
+  currentDate = "January 1, 2016";
+}
+
+ShoppingCart::ShoppingCart(string name, int date)
+{
+  customerName = name;
+  currentDate = date;
+  
+}
+
