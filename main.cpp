09e848eb8c0616875bb5caff5245448c43d87a10
zsmith05 <102535745+zsmith05@users.noreply.github.com>
2024-04-30 13:21:52 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,18 +6,25 @@ using namespace std;
 
 // Zion
 void PrintMenu() {
-   /* Type your code here */
-   
+   cout << "MENU" << endl;
+   cout << "a - Add item to cart" << endl;
+   cout << "d - Remove item from cart" << endl;
+   cout << "c - Change item quantity" << endl;
+   cout << "i - Output items' descriptions" << emdl;
+   cout << "o - Output shopping cart" << endl;
+   cout << "q - Quit" << endl;
 }
 
 void ExecuteMenu(char option, ShoppingCart& theCart) {
-   /* Type your code here */
+
    
 }
 
 int main() {
    string name;
    string date;
+   char option;
+   bool valid = true;
    cout << "Enter customer's name:" << endl;
    cin >> name;
    cout << "Enter today's date:" << endl;
@@ -25,5 +32,25 @@ int main() {
    cout << "Customer's name: " << name << endl;
    cout << "Today's date: " << date << endl;
    ShoppingCart cart = new ShoppingCart(name, date);
+   PrintMenu();
+   
+   cout << "Choose an option: " << endl;
+   cin >> option;
+   if ( option != 'a' || option != 'd' || option != 'c' || option != 'i' || option != 'o' || option != 'q'){
+        valid = false;
+        cout << "Invalid choice" << endl;
+        while(!valid){
+           cin >> option;
+           if (option option == 'a' || option == 'd' || option == 'c' || option == 'i' || option == 'o' || option == 'q'){
+              valid = true;
+           }
+        }
+   }
+   while(option != 'q'){
+      ExecuteMenu(option, cart);
+      PrintMenu();
+      cout << "Choose an option: " << endl;
+      cin >> option;
+   }
    return 0;
 }
